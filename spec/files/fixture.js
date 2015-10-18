04dcfb5ff5002e8dfb5ef5a65a7811f5910e8870
Josa Gesell <josa@gesell.me>
2015-10-18 21:40:35 +0200

    Fix escaping messages
    Fixes #1

--- spec/files/fixture.js
+++ spec/files/fixture.js
@@ -2,6 +2,10 @@ function test() {
   console.log('TODO this is not a todo')
 }
 
+// NOTE Something good to know
 // FIXME Something that has to be done
 // TODO Something that should be done
-// NOTE Something good to know
+// TODO: Something that should be done
+// TODO : Something that should be done
+/* TODO  Something that should be done */
+// TODO
